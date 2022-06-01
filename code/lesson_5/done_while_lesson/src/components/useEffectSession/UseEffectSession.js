0d03fa36fde360439d6ae0755f640d45d442fd90
khatuna120 <shaveshovak@gmail.com>
2022-06-01 23:51:10 +0200

    feat: added files from lesson 5

--- code/lesson_5/done_while_lesson/src/components/useEffectSession/UseEffectSession.js
+++ code/lesson_5/done_while_lesson/src/components/useEffectSession/UseEffectSession.js
@@ -1,8 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 const UseEffectSession = () => {
 
+    /// const [info, setInfo] = useState('posts');
 
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+
+    console.log(posts)
+
+    // console.log('render');
+
+    // useEffect(() => {
+    //     console.log('state is changed');
+    // }, [info])
+
+    // for(var i = 0; i < 100000; i++) {
+
+    // }
+
+    
+    // const fetchData = () => {
+    //     fetch('https://jsonplaceholder.typicode.com/todos')
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+    // }
+
+    // get, post, pull, delete 
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/photos') // 1step I am calling my api, request get
+        .then((response) => response.json())
+        // .then((dataJson) => console.log(dataJson))
+        .then((json) => setPosts(json), setLoading(true))
+    }, [])
+    console.log(posts)
+
+    // const example () => () {
+            // process 
+    // }
+
+    // function examlpe () {
+            // process 
+    // }
+
+    if(loading) {
+        return (
+            <h1>Loading .... </h1>
+        )
+    }
 
     return (
         <>
@@ -11,6 +56,24 @@ const UseEffectSession = () => {
             <button onClick={() => setInfo('posts')}>Click me posts </button> */}
 
             {/* <button onClick={fetchData}>Get data</button> */}
+
+            <section className='post-list'>
+                {
+                    posts.map(post => {
+                        return(
+                            <div className='post-item' key={post.id}>
+                                <h5 style={{
+                                    color: '#fff'
+                                }}>
+                                    <span>{post.id}. </span>
+                                    {post.title}
+                                    <span> user id {post.userId}</span>
+                                </h5>
+                            </div>
+                        )
+                    })
+                }
+            </section>
         </>
     )
 
